57b71536478ebad586412a2a86c0005b1a6da5b9
Peter <balaensipf@gmail.com>
2015-09-13 17:29:08 -0400

    created abstract class SingleFragmentActivity

--- app/src/main/java/com/example/android/popularmovies/SingleFragmentActivity.java
+++ app/src/main/java/com/example/android/popularmovies/SingleFragmentActivity.java
@@ -1,37 +1,34 @@
 package com.example.android.popularmovies;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v7.app.AppCompatActivity;
 
-public class SingleFragmentActivity extends AppCompatActivity {
+
+/**
+ * Created by Peter F Balanesi, @gizmobeta, on 9/1/2015.
+ *
+ * This abstract class was created to DRY out MovieActivity and MovieListActivity. Both Activity
+ * classes host a single fragment. This activity uses a generic container, so that it can be reused
+ * by any implementing fragment.
+ */
+public abstract class SingleFragmentActivity extends AppCompatActivity {
+
+    protected abstract Fragment createFragment();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_single_fragment);
-    }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_single, menu);
-        return true;
-    }
+        FragmentManager fm = getSupportFragmentManager();
+        Fragment fragment = fm.findFragmentById(R.id.fragment_container);
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+        if(fragment == null){
+            fragment = createFragment();
+            fm.beginTransaction().add(R.id.fragment_container,fragment).commit();
         }
-
-        return super.onOptionsItemSelected(item);
     }
+
 }
